1402a83a099c12756563f4556d3d897ca99aa7b3
Роман Чуклинов <xjt@mail.ru>
2022-01-08 09:52:34 +1000

    9. Осталось 3 теста

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -49,10 +49,10 @@ class CommentViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=post_id)
         serializer.save(post=post, author=self.request.user)
 
-    def perform_update(self, instance):
-        if instance.author != self.request.user:
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
-        super().perform_destroy(instance)
+        super().perform_update(serializer)
     
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
