c7c668f1529d14e824930a34dc13f1c7c7bd5aa7
Роман Чуклинов <xjt@mail.ru>
2022-01-11 22:38:35 +1000

    Проект 9 подг к проверке4

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import PermissionDenied
-from posts.models import Group, Post
 from rest_framework import filters, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 
+from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly, IsFollowAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
@@ -15,20 +14,11 @@ User = get_user_model()
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super().perform_destroy(instance)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
